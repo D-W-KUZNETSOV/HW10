948ac842d0408143c451bb1c5ffe940b7085502e
Dmitryi Kuznetsov <d.w.kuznetsov@yandex.ru>
2024-05-13 20:26:19 +0300

    lines

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,15 @@ public class Main {
         String lastName = "Ivanov";
         String fullName = (lastName + " " + firstName + " " + middleName);
 
-        System.out.println("Ф .И .О. сотрудника " + fullName);
-        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        System.out.println("Ф.И.О.сотрудника " + fullName);
+        System.out.println("Данные Ф.И.О.сотрудника для заполнения отчета — " + fullName.toUpperCase());
 
         String firstName1 = "Семён";
         String middleName1 = "Семёнович";
         String lastName1 = "Иванов";
         String fullName1 = (lastName1 + " " + firstName1 + " " + middleName1);
         fullName1 = fullName1.replace("ё", "е");
-        System.out.println("Данные Ф. И. О. сотрудника — " + fullName1);
+        System.out.println("Данные Ф.И.О.сотрудника — " + fullName1);
 
     }
 }
